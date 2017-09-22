3a515673d81c9d2963d24e242ade4ec16d847b1d
ericaddison <ericaddison@gmail.com>
2017-09-22 16:37:11 -0500

    Finished sendmail app, added sendmail README

---
+++ sendMail/README.md
@@ -0,0 +1,6 @@
+# sendMail app
+
+This is a very simple app that sends an email message by specifying the "To", "Subject", and "Body" fields.
+It can send email either by using the AppEngine `send_mail()` method, or with the `EmailMessage` class.
+
+The app is live at: https://ea-ut-apt-sendmail.appspot.com/.
--- sendMail/send_mail.py
+++ sendMail/send_mail.py
@@ -4,7 +4,7 @@ from google.appengine.api import mail
 import webapp2
 
 
-# send an email
+# send an email with the send_mail method
 def send_mail(from_address, to_address, subject, body):
     mail.send_mail(sender=from_address,
                    to=to_address,
@@ -12,26 +12,46 @@ def send_mail(from_address, to_address, subject, body):
                    body=body)
 
 
+# send an email with the EmailMessage class
+def send_mail_class(from_address, to_address, subject, body):
+    message = mail.EmailMessage()
+    message.sender = from_address
+    message.to = to_address
+    message.subject = subject
+    message.body = body
+    message.send()
+
+
 class SendMailHandler(webapp2.RequestHandler):
     def post(self):
 
         # gather variables from the POST request
-        from_address = '{}@appspot.gserviceaccount.com'.format(
+        from_address = 'emailer_dude@{}.appspotmail.com'.format(
             app_identity.get_application_id())
         to_address = self.request.get('to_address')
         subject = self.request.get('subject')
         body = self.request.get('body')
+        send_method = self.request.get('send_style')
+
+        self.response.content_type = 'text/html'
 
         # Send the email
-        send_mail(from_address, to_address, subject, body)
+        if send_method == "method":
+            send_mail(from_address, to_address, subject, body)
+        elif send_method == "class":
+            send_mail_class(from_address, to_address, subject, body)
+        else:
+            self.response.write('Invalid email method')
+            return
 
         # write out the details to the page
-        self.response.content_type = 'text/html'
-        self.response.write('Email details:')
+        self.response.write('Email details:<br><br>')
         self.response.write('From: ' + from_address + '<br><br>')
         self.response.write('To: ' + to_address + '<br><br>')
         self.response.write('Subject: ' + subject + '<br><br>')
-        self.response.write('Body: ' + body + '<br><br>')
+        self.response.write('Body: ' + body + '<br><br><br><br>')
+        self.response.write('Send method: ' + send_method + '<br><br>')
+        self.response.write('<a href="/">Go Back</a>')
 
 
 # define the "app" that will be referenced from app.yaml
